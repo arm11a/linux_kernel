3a2f4653b70e4f56903bd0829660d2adbb3ec29f
Knuth Han <kamase79@daum.net>
2015-01-23 03:51:04 +0900

    build-script fix.
    	modified:   build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,19 +1,35 @@
 #!/bin/sh -e
+NR_CPUS=$(grep "processor"</proc/cpuinfo | wc -l)
+PWD=$(pwd)
 
 export ARCH=arm
 export CROSS_COMPILE=arm-linux-gnueabihf-
 
-#cd linux_kernel
-##git checkout master
-#git pull #iamrootArm11A master
-cd linux-3.12.20
-#make exynos_defconfig
-#kmake menuconfig
+cd $PWD/linux-3.12.20
 
-#make -j4
+#------ recovery original source and update. -----
+#git checkout master
+#git pull
+#-------------------------------------------------
 
-export KBUILD_SRC=${pwd}
+if [ ! -e $PWD/linux-3.12.20/.config ]; then
+    make exynos_defconfig
+fi
 
-make tags
-make cscope
+make -j$NR_CPUS
 
+#
+# uncommnet your tagging tool.
+# ex) gtags, tags, and cscope.
+
+if [ -e /usr/local/bin/gtags ]; then
+    make gtags
+fi
+
+if [ -e /usr/bin/ctags ]; then
+    make tags
+fi 
+
+if [ -e /usr/bin/cscope ]; then
+    make cscope
+fi
